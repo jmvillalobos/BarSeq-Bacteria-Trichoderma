2a09e1eac3839ca0ff088e0ac1fc668a1a3641f7
José Manuel Villalobos Escobedo <jvillalobos886@gmail.com>
2022-10-06 12:08:18 -0700

    Add files via upload

---
+++ script_scatter_WTHerbaspirillum.R
@@ -0,0 +1,84 @@
+#analysis BarSeq for Herbaspirillum
+heat=read.table("/Users/manuel/Documents/posdoc_berkeley/barseq_results/tabla_herbas.txt", header=TRUE, row.names = 1, sep="\t")
+colnames(heat)
+heat[,"SumSpend"]= rowSums(heat[,c("A","B", "C")])/3
+heat[,"control"]=rowSums(heat[,c("D","E","F")])/3
+
+genesFitdownWT=rownames(heat)[heat$SumSpend < -1.5 & heat$control > -1]
+genesFitupWT=rownames(heat)[heat$SumSpend > 1.5 & heat$control < 1]
+genesFitComple=rownames(heat)[heat$SumSpend < 1 & heat$control < -2& heat$SumSpend  > -2]
+
+
+length(genesFitdownWT)
+length(genesFitupWT)
+length(genesFitComple)
+
+fitnenegWT=heat[genesFitdownWT,]
+fitneposiWT=heat[genesFitupWT,]
+fitneComple=heat[genesFitComple,]
+
+
+
+comple=c( "HSERO_RS05910",
+          "HSERO_RS07625",
+          "HSERO_RS15630",
+          "HSERO_RS16070",
+          "HSERO_RS19995",
+          "HSERO_RS21035")
+
+
+
+genes_interIron=c("HSERO_RS02660",
+                  "HSERO_RS03450",
+                  "HSERO_RS00165",
+                  "HSERO_RS03440",
+                  "HSERO_RS13650",
+                  "HSERO_RS03445")
+
+
+
+genes_interABC=c( 
+  "HSERO_RS10850"
+)
+
+
+
+genes_interArn=c("HSERO_RS21375")
+
+
+
+fitnenegNamesIron=heat[genes_interIron,]
+fitnenegNamesABC=heat[genes_interABC,]
+fitnenegNamesArn=heat[genes_interArn,]
+
+
+
+#general scatter plot
+plot( heat$SumSpend, heat$control,  col="darkgray", xlim = c(-4,1), ylim = c(-4,1), cex = 0.7,pch=19, 
+      xlab = "T. atroviride WT-Spent media", ylab= "Without Fungus", main = "Herbaspirillum seropedicae SmR1")
+points(fitnenegWT$SumSpend, fitnenegWT$control, col="purple", pch=19,cex = 0.7  )
+points(fitneposiWT$SumSpend, fitneposiWT$control, col="red", pch=19,cex = 0.7 )
+points(fitneComple$SumSpend, fitneComple$control, col="blue", pch=19, cex = 0.7  )
+#points for annotation
+points(fitnenegNamesIron$SumSpend,fitnenegNamesIron$control, col="coral3", pch=19, cex = 1.2 )
+points(fitnenegNamesABC$SumSpend,fitnenegNamesABC$control, col="green", pch=19, cex = 1.2 )
+points(fitnenegNamesArn$SumSpend,fitnenegNamesArn$control, col="brown", pch=19, cex = 1.2 )
+
+#text for ids
+text(fitnenegWT$SumSpend, fitnenegWT$control, labels = row.names(fitnenegWT),col="black", cex=0.5, pos=3 )
+
+text(fitnenegNamesIron$SumSpend,fitnenegNamesIron$control,labels = row.names(fitnenegNamesIron),col="black", cex=0.5, pos=3 )
+text(fitnenegNamesABC$SumSpend,fitnenegNamesABC$control, labels = row.names(fitnenegNamesABC),col="black", cex=0.5, pos=3 )
+text(fitnenegNamesArn$SumSpend,fitnenegNamesArn$control, labels = row.names(fitnenegNamesArn),col="black", cex=0.5, pos=3 )
+text(fitneComple$SumSpend,fitneComple$control, labels = row.names(fitneComple),col="black", cex=0.5, pos=3 )
+
+legend(-1, -3, legend=c("Negative Fitness","Recovered","Iron", "ABC", "Arn-operon"),
+       col=c("purple","blue","coral3", "green", "brown"), pch=19, cex=1.2 )
+
+abline(h=0, col="black", lty=2, lwd=1)
+abline(v=0, col="black", lty=2, lwd=1)
+abline(-0.01,1)
+
+
+
+Exporter as a PDF using 7X7
